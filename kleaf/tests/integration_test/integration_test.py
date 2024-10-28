MiCode/kernel_build

commit 6af2b8a169e717b31af365779940531b38d19712

kleaf: Re-declare KERNEL_DIR under KLEAF_REPO_WORKSPACE_ROOT (2nd attempt)
If KLEAF_REPO_WORKSPACE_ROOT is set, i.e. when
@kleaf is a dependent module, set KERNEL_DIR to
be under KLEAF_REPO_WORKSPACE_ROOT. That is, set it
to something like external/kleaf~/common.

Test: TH
Bug: 291918087
Change-Id: Id4996548b10cd9d20dab510167e487b2863675e6

kleaf/impl/kernel_env.bzl
@@ -360,6 +360,7 @@ def _get_env_setup_cmds(ctx):
         pre_env += debug.trap()
 
     kleaf_repo_workspace_root = Label(":kernel_env.bzl").workspace_root
+    kleaf_repo_workspace_root_slash = (kleaf_repo_workspace_root + "/") if kleaf_repo_workspace_root else ""
 
     pre_env += """
         # KLEAF_REPO_WORKSPACE_ROOT: workspace_root of the Kleaf repository. See Label.workspace_root.
@@ -406,6 +407,11 @@ def _get_env_setup_cmds(ctx):
             export dtstree=$(realpath -s $(dirname ${{DTSTREE_MAKEFILE}}) --relative-to ${{ROOT_DIR}}/${{KERNEL_DIR}})
         fi
 
+        # Redeclare KERNEL_DIR to be under $KLEAF_REPO_WORKSPACE_ROOT.
+        if [ -n "${{KLEAF_REPO_WORKSPACE_ROOT}}" ]; then
+            export KERNEL_DIR=${{KLEAF_REPO_WORKSPACE_ROOT:+$KLEAF_REPO_WORKSPACE_ROOT/}}${{KERNEL_DIR#{kleaf_repo_workspace_root_slash}}}
+        fi
+
         ## Set up KCPPFLAGS
 
         # Replace ${{ROOT_DIR}} with "/proc/self/cwd" in the file name
@@ -431,6 +437,7 @@ def _get_env_setup_cmds(ctx):
     """.format(
         get_make_jobs_cmd = status.get_volatile_status_cmd(ctx, "MAKE_JOBS"),
         linux_x86_libs_path = ctx.files._linux_x86_libs[0].dirname,
+        kleaf_repo_workspace_root_slash = kleaf_repo_workspace_root_slash,
     )
     return struct(
         pre_env = pre_env,

cachiusa/android_kernel_build

commit 4da8f3a177fcadfb8d6084764b857e2bad1ee50c

kleaf: Fix DDKv2 with separate kernel source tree
With DDKv2 set up, i.e. @kleaf is a dependent module, if a
a kernel_build() sets KERNEL_DIR to a separate kernel source tree to
build kernel modules or dtbs, it should be allowed. Before this change,
it was incorrectly assumed that the KERNEL_DIR must be below @kleaf.

Implementation details: This change makes it so that we ONLY fix up the
value of KERNEL_DIR from downloaded prebuilt scripts when using
kernel_filegroup(). When using kernel_build(), we assume that the user
has provided the correct KERNEL_DIR, so we don't change the value.

Test: integration test
Bug: 375647893
Fixes: 6af2b8a169e7
    ("kleaf: Re-declare KERNEL_DIR under KLEAF_REPO_WORKSPACE_ROOT (2nd attempt)")
Change-Id: I565c0e95afbde412fb664458023260c31eedff56

kleaf/impl/kernel_env.bzl
@@ -407,7 +407,6 @@ def _get_env_setup_cmds(ctx):
         pre_env += debug.trap()
 
     kleaf_repo_workspace_root = Label(":kernel_env.bzl").workspace_root
-    kleaf_repo_workspace_root_slash = (kleaf_repo_workspace_root + "/") if kleaf_repo_workspace_root else ""
 
     pre_env += """
         # KLEAF_REPO_WORKSPACE_ROOT: workspace_root of the Kleaf repository. See Label.workspace_root.
@@ -464,8 +463,27 @@ def _get_env_setup_cmds(ctx):
         fi
 
         # Redeclare KERNEL_DIR to be under $KLEAF_REPO_WORKSPACE_ROOT.
-        if [ -n "${{KLEAF_REPO_WORKSPACE_ROOT}}" ]; then
-            export KERNEL_DIR=${{KLEAF_REPO_WORKSPACE_ROOT:+$KLEAF_REPO_WORKSPACE_ROOT/}}${{KERNEL_DIR#{kleaf_repo_workspace_root_slash}}}
+        # Only do that if all of the following is true:
+        #   - We are setting up the variables for a kernel_filegroup with prebuilt scripts
+        #     (KLEAF_FIX_KERNEL_DIR == 1)
+        #   - The kernel_build() that originally built these prebuilts was declared at the
+        #     root Bazel module in its workspace (we may relax this requirement in the future if
+        #     there's a use case)
+        # A typical use case for this is that KERNEL_DIR=common in prebuilt scripts, but if @kleaf
+        #    is a dependent module and common/ is below @kleaf, then kernel_filegroup need to fix
+        #    the value so that KERNEL_DIR=external/kleaf~/common.
+        if [ "${{KLEAF_FIX_KERNEL_DIR}}" = 1 ]; then
+
+            if [ -n "{kernel_build_workspace_root}" ]; then
+                echo "ERROR: The original kernel_build() that built these prebuilts was " >&2
+                echo "    {kernel_build_label}" >&2
+                echo "  It is currently not supported to use these prebuilts within a kernel_filegroup." >&2
+                echo "  Instead, the kernel_build() should have been built at the root Bazel module." >&2
+                echo "  Please contact the provider for these prebuilts to resolve this error." >&2
+                exit 1
+            fi
+
+            export KERNEL_DIR=${{KLEAF_REPO_WORKSPACE_ROOT:+$KLEAF_REPO_WORKSPACE_ROOT/}}${{KERNEL_DIR}}
         fi
 
         ## Set up KCPPFLAGS and KCPPFLAGS_COMPAT
@@ -498,8 +516,10 @@ def _get_env_setup_cmds(ctx):
     """.format(
         get_make_jobs_cmd = status.get_volatile_status_cmd(ctx, "MAKE_JOBS"),
         get_make_keep_going_cmd = status.get_volatile_status_cmd(ctx, "MAKE_KEEP_GOING"),
-        kleaf_repo_workspace_root_slash = kleaf_repo_workspace_root_slash,
+        kernel_build_workspace_root = ctx.label.workspace_root,
+        kernel_build_label = str(ctx.label).removesuffix("_env"),
     )
+
     return struct(
         pre_env = pre_env,
         post_env = post_env,

kleaf/impl/kernel_filegroup.bzl
@@ -110,6 +110,7 @@ def _get_config_env(ctx):
     env_setup_command = """
         KLEAF_REPO_WORKSPACE_ROOT={kleaf_repo_workspace_root}
         KLEAF_HERMETIC_BASE={hermetic_base}
+        KLEAF_FIX_KERNEL_DIR=1
     """.format(
         kleaf_repo_workspace_root = Label(":kernel_filegroup.bzl").workspace_root,
         hermetic_base = hermetic_tools.internal_hermetic_base,

kleaf/tests/integration_test/ddk_workspace_test/fake_common.BUILD
@@ -1,0 +1,29 @@
+load(
+    "@kleaf//build/kernel/kleaf:kernel.bzl",
+    "kernel_build",
+)
+
+filegroup(
+    name = "common_kernel_sources",
+    srcs = glob(
+        ["**"],
+        exclude = [
+            "BUILD.bazel",
+            "**/*.bzl",
+            ".git/**",
+        ],
+    ),
+    visibility = ["//visibility:public"],
+)
+
+# Pretend that we have a kernel_build to build some in-tree modules.
+kernel_build(
+    name = "fake_device",
+    srcs = [":common_kernel_sources"],
+    # Force the target to be built by Bazel.
+    outs = [".config"],
+    build_config = "build.config.common",
+    defconfig = ":arch/arm64/configs/gki_defconfig",
+    make_goals = ["olddefconfig"],
+    makefile = ":Makefile",
+)

kleaf/tests/integration_test/integration_test.py
@@ -635,6 +635,29 @@ class DdkWorkspaceSetupTest(KleafIntegrationTestBase):
             return
         self._run_ddk_workspace_setup_test(kleaf_repo, self.ddk_workspace)
 
+    def test_build_kernel_at_root_module(self):
+        """Tests that kernel_build() at the root module is still functional.
+
+        See b/375647893"""
+        kleaf_repo = self.ddk_workspace / "external/kleaf"
+        if not arguments.mount_spec:
+            common_build = pathlib.Path(self._common()) / "BUILD.bazel"
+            self.restore_file_after_test(common_build)
+            # pylint: disable=line-too-long
+            shutil.copy(
+                "build/kernel/kleaf/tests/integration_test/ddk_workspace_test/fake_common.BUILD",
+                common_build)
+            mount_spec = {
+                self.real_kleaf_repo: kleaf_repo,
+                self.real_kleaf_repo / self._common(): self.ddk_workspace / "forked_common",
+            }
+            self._unshare_mount_run(mount_spec=mount_spec, link_spec=LinkSpec())
+            return
+        self._run_ddk_workspace_setup_test(
+            kleaf_repo,
+            ddk_workspace=self.ddk_workspace,
+            build_targets=["//forked_common:fake_device"])
+
     def test_setup_with_local_prebuilts(self):
         """Tests that init_ddk --prebuilts_dir & --local works."""
         if not arguments.mount_spec:
@@ -720,7 +743,21 @@ class DdkWorkspaceSetupTest(KleafIntegrationTestBase):
                                       local: bool = True,
                                       url_fmt: str | None = None,
                                       build_id: str | None = None,
-                                      sync: bool | None = None):
+                                      sync: bool | None = None,
+                                      build_targets: Iterable[str] = ()):
+        """Tests a DDKv2 workspace setup.
+
+        Args:
+            kleaf_repo: path to @kleaf module.
+            ddk_workspace: path to root of workspace.
+            prebuilts_dir: See init_ddk.py
+            local: See init_ddk.py
+            url_fmt: See init_ddk.py
+            build_id: See init_ddk.py
+            sync: See init_ddk.py
+            build_targets: If not empty, build the given list of targets below
+                the workspace. Otherwise run build tests.
+        """
         # kleaf_repo relative to ddk_workspace
         kleaf_repo_rel = self._force_relative_to(
             kleaf_repo, ddk_workspace)
@@ -777,8 +814,12 @@ class DdkWorkspaceSetupTest(KleafIntegrationTestBase):
         # Switch base kernel when using prebuilts
         if prebuilts_dir:
             args.append("--//tests:kernel=@gki_prebuilts//kernel_aarch64")
-        args.append("//tests")
-        self._check_call("test", args, cwd=ddk_workspace)
+        if build_targets:
+            args.extend(build_targets)
+            self._check_call("build", args, cwd=ddk_workspace)
+        else:
+            args.append("//tests")
+            self._check_call("test", args, cwd=ddk_workspace)
 
         # Delete generated files
         self._check_call("clean", ["--expunge"], cwd=ddk_workspace)