e15717763ff3fa9b51dbc58b8c3c682bfef34296
cachiusa <nhat.dogpro@outlook.com>
2024-11-30 13:40:22 +0000

    m

--- build-tools/llvm_cbl.sh
+++ build-tools/llvm_cbl.sh
@@ -17,10 +17,10 @@ HOST_ARCH=$(uname -m)
 TARGET_ARCH="aarch64"
 COMP="xz"
 VERSION_PTN='[0-9]+\.[0-9]+\.[0-9]+(-rc[0-9]+)?'
-VERSION=
 
 #---- dynamic variables ----
 set_vars() {
+    [[ $1 = "--empty-ver" ]] && local VERSION=
     case ${TYPE} in
         llvm)
             FILES="${MIRROR}/pub/tools/llvm/files"
@@ -29,33 +29,34 @@ set_vars() {
             TAR_ARGS="--strip-components=1 ${PKG}"
         ;;
         gcc)
-            SUBDIR1="gcc-${VERSION}-nolibc"
-            SUBDIR2="${TARGET_ARCH}-linux"
+            GCC_1="gcc-${VERSION}-nolibc"
+            GCC_2="${TARGET_ARCH}-linux"
 
             FILES="${MIRROR}/pub/tools/crosstool/files/bin/${HOST_ARCH}/${VERSION}"
-            PKG="${HOST_ARCH}-${SUBDIR1}-${SUBDIR2}"
-            COMPILER="${SUBDIR2}-gcc"
-            TAR_ARGS="--strip-components=2 ${SUBDIR1}/${SUBDIR2}"
+            PKG="${HOST_ARCH}-${GCC_1}-${GCC_2}"
+            COMPILER="${GCC_2}-gcc"
+            TAR_ARGS="--strip-components=2 ${GCC_1}/${GCC_2}"
 
-            [[ -n ${gcc_alt_pkg} ]] && PKG="${HOST_ARCH}-${SUBDIR1}_${SUBDIR2}"
+            [[ -n ${gcc_alt_pkg} ]] && PKG="${HOST_ARCH}-${GCC_1}_${GCC_2}"
     esac
     TAR="${PKG}.tar${COMP}"
     TAR_URL="${FILES}/${TAR}"
-    INSTALLDIR="${HOME_DIR}/${TYPE}/${VERSION}"
+    INSTALL_DIR_BASE="${HOME_DIR}/${TYPE}"
     DL_DIR="${HOME_DIR}/pkgs"
 }
 #---- functions ----
 set_colors() {
     if [[ -n ${GITHUB_ACTION} ]] || [[ -t 1 ]]; then
-        _lblue='\e[38;5;14m'
-        _blue='\e[38;5;6m'
+        _lblue='\e[38;5;51m'
+        _blue='\e[38;5;39m'
         _bold='\e[1m'
-        _lred='\e[38;5;9m'
-        _red='\e[38;5;1m'
+        _lred='\e[38;5;196m'
+        _red='\e[38;5;124m'
         _restore='\e[0m'
     fi
 }
 info_msg() { echo -e "${_blue}[${_lblue}+${_blue}]${_restore} ${1}"; }
+info_msg2() { echo -e " +  ${1}"; }
 error_msg() { echo -e "${_bold}${_red}[${_lred}!${_red}]${_restore}${_bold} ${1}${_restore}"; exit 1; }
 help_msg() { echo -e "Usage: $0 -p <install path> [options...]
  -a <arch>      host arch
@@ -65,11 +66,11 @@ help_msg() { echo -e "Usage: $0 -p <install path> [options...]
  -f <format>    set tar compression format
         default: xz
  -v <version>   specify toolchain version
+    'all': download all versions (please do not DDoS the servers)
+    'stable': ignore '-rc' versions
         default: latest
-        if set to 'all', download all versions (please do not DDoS the servers)
  -x             print each command being executed
  --no-extract   downloads the package but don't extract
- --stable       ignore '-rc' versions
  --gcc          set up GCC and binutils
  --llvm         set up Clang/LLVM (default)
  --help         this screen"
@@ -86,53 +87,64 @@ parse_args() {
             -A) TARGET_ARCH="$value"    ;;
             -f) COMP="$value"           ;;
             -p) HOME_DIR="$value"       ;;
-            -v) VERSION="$value"        ;;
+            -v) SET_VER="$value"        ;;
             -x) set -o xtrace           ;;
             --help) help_msg ; exit 0   ;;
             --gcc) TYPE="gcc"           ;;
             --llvm) TYPE="llvm"         ;;
             --no-extract) NO_EXTRACT=1  ;;
-            --stable)       ;;
         esac
         shift
     done
 
-    COMP=".${COMP}"
+    if [[ -n ${COMP} ]]; then
+        COMP=".${COMP}"
+    else
+        COMP=
+    fi
 
-    case ${VERSION} in
-        all) GET_ALL=1 ;;
+    case ${SET_VER:-latest} in
+        latest) VERSION= ;;
         stable) STABLE_VER=1 ;;
-        latest)
+        all) GET_ALL=1 ;;
+        *) VERSION=${SET_VER}
     esac
 
     HOME_DIR=$(readlink -m "${HOME_DIR}")
+
     [[ -z ${HOME_DIR} ]] && error_msg "You must set toolchain home with '-p'"
 }
 print_info() {
-    info_msg "Install path: ${HOME_DIR}"
+    info_msg2 "Toolchain: ${TYPE}"
+    info_msg2 "Install home: ${HOME_DIR}"
+    info_msg2 "Host arch: ${HOST_ARCH}"
 
-    info_msg "Host arch: ${HOST_ARCH}"
+    [[ ${TYPE} = "gcc" ]] && info_msg2 "Target arch: ${TARGET_ARCH}"
 
-    [[ ${TYPE} = "gcc" ]] && info_msg "GCC target arch: ${TARGET_ARCH}"
-
-    mkdir -p "${HOME_DIR}" || error_msg "The install path is not writable!"
+    mkdir -p "${HOME_DIR}" || error_msg "The home path is not writable!"
+}
+cc2ver() {
+    [[ -f $1 ]] || return
+    "$1" --version | grep -oE "${VERSION_PTN}" | head -n 1
 }
 check_installed() {
-    if [[ -z ${installed_vers} ]]; then
-        installed_ccs=$(find "${HOME_DIR}" -wholename "*bin/${COMPILER}")
-    fi
+    # check if $VERSION specified is installed in install dir
+    
+    IS_INSTALLED=
+    installed_ccs=$(find -P "${HOME_DIR}" -path "*bin/${COMPILER}")
 
     for cc in ${installed_ccs}; do
-        INSTALLED_VER=$(echo "$cc" | grep -oE "${VERSION_PTN}")
-
-        if [[ ${INSTALLED_VER} = "${VERSION}" ]] && "$cc" --version ; then
-            local exit=1
-            break
+        if "$cc" --version &> /dev/null ; then
+            INSTALLED_VER=$(cc2ver "$cc")
+            if [[ ${INSTALLED_VER} = "${VERSION}" ]]; then
+                INSTALLED_DIR=$(dirname "${cc}")
+                IS_INSTALLED=1 && break
+            fi
         fi
     done
 
-    if [[ ${exit} = "1" ]]; then
-        info_msg "${TYPE} ${VERSION} is installed"
+    if [[ ${IS_INSTALLED} = "1" ]]; then
+        info_msg2 "Installed: ${TYPE} ${VERSION}"
         return
     else
         return 1
@@ -141,8 +153,7 @@ check_installed() {
 get_versions() {
     info_msg "Fetching versions list"
 
-    local VERSION=
-    set_vars
+    set_vars --empty-ver
 
     _vers="$(curl -s "${FILES}/" | grep -oE "${VERSION_PTN}" | sort -uV)"
 
@@ -156,6 +167,11 @@ get_versions() {
     done
 
     LATEST_VER=${CURRENT_VERS[-1]}
+
+    if [[ -z ${VERSION} ]]; then
+        info_msg2 "Latest version: ${LATEST_VER}"
+        VERSION=${LATEST_VER}
+    fi
 }
 check_and_set_version() {
     if [[ -n ${VERSION} ]]; then
@@ -169,15 +185,6 @@ check_and_set_version() {
 
         [[ -z ${is_ver_ok} ]] && error_msg "${TYPE} ${VERSION}: not found
 Avaliable versions:${_restore} ${CURRENT_VERS[*]}"
-
-    else
-        VERSION=${LATEST_VER}
-
-        if [[ ${LATEST_VER} = "${INSTALLED_VER}" ]]; then
-            info_msg "Already up to date." && exit 0
-        else
-            info_msg "Latest version: ${LATEST_VER}"
-        fi
     fi
 
     if [[ ${TYPE} = "gcc" ]]; then
@@ -208,10 +215,10 @@ get_pkg() {
     [[ -f ${DL_DIR}/${TAR} ]] || error_msg "Download failed"
 }
 hash_pkg() {
-    info_msg "Verifying: ${TAR}"
+    info_msg "Verifying sha256"
 
     if sha256sum "${DL_DIR}/${TAR}" | grep -q "${CURRENT_SHA256}" ; then
-        info_msg "sha256: ok"
+        info_msg2 "sha256: OK"
     else
         error_msg "sha256: mismatch"
     fi
@@ -221,14 +228,14 @@ extract() {
 
     info_msg "Extracting package"
 
-    mkdir -p "${INSTALLDIR}"
+    mkdir -p "${INSTALL_DIR_BASE}"
 
     # shellcheck disable=SC2086
-    tar -C "${INSTALLDIR}" \
+    tar -C "${INSTALL_DIR_BASE}" \
         -f "${DL_DIR}/${TAR}" \
         -x \
         -a \
-        ${TAR_ARGS}
+        #${TAR_ARGS}
 }
 work_now() {
     check_installed || {
@@ -239,7 +246,27 @@ work_now() {
         check_installed
     }
 }
+link_dir() {
+    target_bin=${HOME_DIR}/bin
 
+    if [[ ${IS_INSTALLED} != "1" ]]; then
+        return 1
+    elif [[ ${INSTALLED_VER} = "$(cc2ver "${target_bin}/${COMPILER}")" ]]; then
+        return
+    fi
+
+    info_msg "Symlinking ${INSTALLED_DIR} to ${target_bin}"
+
+    installed_dir_rel=$(realpath --relative-to="${HOME_DIR}" "${INSTALLED_DIR}")
+
+    if [[ -h ${target_bin} ]]; then
+        unlink "${target_bin}"
+    elif [[ -d ${target_bin} ]]; then
+        rm -rf "${target_bin}"
+    fi
+
+    ln -sf "${installed_dir_rel}" "${target_bin}"
+}
 #---- main ----
 set_colors
 parse_args "$@"
@@ -248,9 +275,9 @@ print_info
 get_versions
 
 if [[ -n ${GET_ALL} ]]; then
-    for VERSION in "${CURRENT_VERS[@]}"; do
-        work_now
-    done
+    for VERSION in "${CURRENT_VERS[@]}"; do work_now ; done
 else
     work_now
+    link_dir
 fi
+info_msg "Done."
